298c61ebb7ea91b3f28ed6144485b98293ceb036
Ashishkalbande11 <ashishkalbande6@gmail.com>
2024-03-13 12:19:18 +0530

    Encapsulation Assignment

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public String setName(){
+        return name;
+    }
+    public void getName(String name){
+        this.name = name;
+    }
 }
